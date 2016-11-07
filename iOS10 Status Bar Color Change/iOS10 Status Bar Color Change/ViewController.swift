b10585fc41a5002547b6c0b9c522a6ee280658f3
san2ride <sanchezjason@me.com>
2016-11-06 18:38:16 -0700

    iOS10 AppDelegate application color change

---
+++ iOS10 Status Bar Color Change/iOS10 Status Bar Color Change/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  iOS10 Status Bar Color Change
+//
+//  Created by don't touch me on 11/6/16.
+//  Copyright © 2016 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+       
+    }
+
+   
+
+}
+
